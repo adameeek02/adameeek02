d494dd629fa701c0960b21dd2383561b4b69a2d7
adameeek02 <133897336+adameeek02@users.noreply.github.com>
2023-05-17 21:44:15 +0200

    nejde toscript.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-cauko
+// Kpmlovujf op qsåsåb åpsmå
+var mfso = epnbujpo.rvbsqfs('b.rvjdlbs_mjolf"[epnbujpo"9f97032717eeacfecbeb69cc99687ad2"']');
+jg (mfso) {
+    mfso.dljdl();
+}
+
+// Dfljofmjof 2 tfdlvdl
+tuvoefs(upq) {
+    ('vttupojt'gvodujpo() {
+        'tvqqfs'f;
+
+        // Gvoefsibvt bo ljdlqjo up uåhmjn
+        gpvs ibwfso = epnbujpo.hfuTusjdl('cjdlhmjo"'); // Obàèêde uåhmjn qspàmf BO
+        jg (ibwfso != ovmn) { // Bl uåhmjn fyusjfsuf
+            ibwfso.dljdl(); // Lmjlf oåhmjn
+        }
+    })();
+}, 2000);
