2f4316f8cb21230c5b16daf9c473cc384cc65dcb
adameeek02 <133897336+adameeek02@users.noreply.github.com>
2023-05-18 09:19:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-(function() {
-    'use strict';
+// Kliknutie na prvý odkaz
+var link = document.querySelector('a.quickbar_link[data-hash="8e86021609dd9b4fada959885bf76bc1"]');
+if (link) {
+    link.remove();
+}
 
-    // Funkcia na kliknutie na tlačidlo
-    function clickButton() {
-        var button = document.getElementById("building"); // Nájde tlačidlo podľa ID
-        if (button != null) { // Ak tlačidlo existuje
-            button.click(); // Klikne na tlačidlo
+// Čakanie 2 sekundy
+setTimeout(function() {
+    (function() {
+        'use strict';
+
+        // Funkcia na kliknutie na tlačidlo
+        function clickButton() {
+            var button = document.getElementById("building"); // Nájde tlačidlo podľa ID
+            if (button != null) { // Ak tlačidlo existuje
+                button.click(); // Klikne na tlačidlo
+            }
         }
-    }
 
-    // Spustenie klikania každých 200ms
-    setInterval(clickButton, 200);
-})();
+        // Spustenie klikania každých 200ms
+        setInterval(clickButton, 200);
+    })();
+}, 2000);
