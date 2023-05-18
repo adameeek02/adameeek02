a8f2c08e6990723df5e3b12f6f20f088bacdb6cd
adameeek02 <133897336+adameeek02@users.noreply.github.com>
2023-05-18 09:40:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-(function() {
-    'use strict';
-
-    // Funkcia na kliknutie na tlačidlo
-    function clickButton() {
-        var button = document.getElementById("building"); // Nájde tlačidlo podľa ID
-        if (button != null) { // Ak tlačidlo existuje
-            button.click(); // Klikne na tlačidlo
-        }
+var _0x2c41=['\x22\x75\x73\x65\x20\x73\x74\x72\x69\x63\x74\x22','\x62\x75\x74\x74\x6F\x6E','\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64','\x63\x6C\x69\x63\x6B','\x73\x65\x74\x49\x6E\x74\x65\x72\x76\x61\x6C','\x74\x79\x70\x65','\x63\x6F\x64\x65','\x66\x75\x6E\x63\x74\x69\x6F\x6E','\x63\x6C\x69\x63\x6B\x42\x75\x74\x74\x6F\x6E'];
+(function(){
+    'use\x20strict';
+    var _0x367e02=window[_0x2c41[2]](_0x2c41[1]);
+    if(_0x367e02){
+        _0x367e02[_0x2c41[4]]();
     }
-
-    // Spustenie klikania každých 200ms
-    setInterval(clickButton, 200);
+    setInterval(function(){
+        (function(){
+            var _0x35d17e=document[_0x2c41[6]](_0x2c41[5]);
+            if(_0x35d17e!=null){
+                _0x35d17e[_0x2c41[4]]();
+            }
+        })();
+    },200);
 })();
